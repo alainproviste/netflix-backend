bbf14aff02617b985c944c907af979441b25a67c
Rémi KARMANN <remikarmann@hotmail.com>
2022-02-28 23:28:31 +0100

    correction

--- src/controllers/wishlist.controller.js
+++ src/controllers/wishlist.controller.js
@@ -1,4 +1,4 @@
-const Wishlist = require("../models/Wishlist.model");
+const Wishlist = require("../models/wishlist.model");
 
 exports.getWishlist = (req, res) => {
   Wishlist.findOne({ user: req.body.user })
